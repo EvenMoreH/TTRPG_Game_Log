2cdafe4709d7a9477c1c0689d4887081869894a8
EvenMoreH <hubert.lewan@gmail.com>
2024-12-26 19:00:56 +0100

    innitial repo setup

---
+++ app/main.py
@@ -0,0 +1,6 @@
+from fasthtml.common import * # type: ignore
+# from fasthtml.common import (Form, Fieldset, Label, Input, Button, Html, Head, Body, Div, P, Title, Titled, A, Link)
+import re
+
+app, rt = fast_app(static_path="static") # type: ignore
+
